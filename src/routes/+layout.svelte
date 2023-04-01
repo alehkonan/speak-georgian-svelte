d7cb0c05e722a3a5b6b1200eef5a06ac926554ee
Aleh Konan <olegkonanff@gmail.com>
2023-04-01 16:06:46 +0400

    feat: switch to svelte kit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import './styles.css';
+</script>
+
+<div>
+	<main>
+		<slot />
+	</main>
+</div>
+
+<style>
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="The web application" />
+</svelte:head>
+
+<main>
+	<h1>Hello</h1>
+</main>
+
+<style>
+</style>
